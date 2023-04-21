3a7af95ad403bfc2f2771f1033b1cf3a0d7f5c5a
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-04-21 15:31:31 +0200

    register account ok!

---
+++ src-js/js/api/constants.mjs
@@ -0,0 +1,14 @@
+export const API_HOST_URL = "https:/api.noroff.dev";
+export const API_BASE_URL = "/api/v1";
+export const API_AUCTION_BASE = "/auction";
+export const API_AUCTION_URL = `${API_HOST_URL}${API_BASE_URL}${API_AUCTION_BASE}`;
+
+
+
+
+
+
+export const listingsEnded = document.querySelector(".endedListings");
+export const listingsActive = document.querySelector(".activeListings");
+export const myListingsOnly = document.querySelector(".myListingsOnly");
+export const allListings = document.querySelector(".all-listings");
