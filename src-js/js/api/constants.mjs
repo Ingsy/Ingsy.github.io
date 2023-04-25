028919e7448f4b125bc5d35f8daa18ce43ca8bae
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-04-25 09:18:20 +0200

    Update constants.mjs

--- src-js/js/api/constants.mjs
+++ src-js/js/api/constants.mjs
@@ -5,10 +5,16 @@ export const API_AUCTION_URL = `${API_HOST_URL}${API_BASE_URL}${API_AUCTION_BASE
 
 
 
-
+export const searchForm = document.querySelector("#searchform");
+export const searchInput = document.querySelector("#searchInput");
 
 
 export const listingsEnded = document.querySelector(".endedListings");
 export const listingsActive = document.querySelector(".activeListings");
 export const myListingsOnly = document.querySelector(".myListingsOnly");
 export const allListings = document.querySelector(".all-listings");
+
+export const ProfileName = document.querySelector("#ProfileName");
+export const userName = localStorage.getItem("userName");
+export const Avatar = document.querySelector("#avatar");
+export const avatar = localStorage.getItem("avatar");
