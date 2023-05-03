d6b73ca18f8bddbf53893a7b1e060c320e633da8
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-05-03 20:30:04 +0200

    Update constants.mjs

--- js/api/constants.mjs
+++ js/api/constants.mjs
@@ -7,16 +7,18 @@ export const API_AUCTION_URL = `${API_HOST_URL}${API_BASE_URL}${API_AUCTION_BASE
 const _seller = "/listings";
 export const getSellerURL = `${API_AUCTION_URL}${_seller}?_seller=true&_bids=true`;
 
+export const activeListingsURL = `${API_AUCTION_URL}${_seller}?_active=true`;
+
+
 
 export const searchForm = document.querySelector("#searchform");
 export const searchInput = document.querySelector("#searchInput");
 
 
-//export const listingsWithImg = document.querySelector(".WithImg");
-//export const listingsWithNoImg = document.querySelector(".NoImg");
+
 export const listingsEnded = document.querySelector(".endedListings");
-export const listingsActive = document.querySelector(".activeListings");
-export const myListingsOnly = document.querySelector(".myListingsOnly");
+export const listingsActive = document.querySelector("#activeListings");
+export const myListingsOnly = document.querySelector("#myListingsOnly");
 export const allListingsbtn = document.querySelector(".all-listings");
 
 export const ProfileName = document.querySelector("#ProfileName");
@@ -28,9 +30,9 @@ export const Credits = localStorage.getItem("credits")
 
 
 
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+export const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 // eslint-disable-next-line no-unused-vars, no-undef
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+export const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
 
 //profile
