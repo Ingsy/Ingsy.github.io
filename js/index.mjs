1c5a5190f5e9c89d66ef05e73f9a6c1dbfab1c5f
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-05-10 11:25:41 +0200

    Update index.mjs

--- js/index.mjs
+++ js/index.mjs
@@ -2,8 +2,10 @@ import * as listeners from "./handlers/index.mjs";
 import * as LOGIN from "./handlers/login.mjs";
 import { getProfileListings } from "../js/api/profile.mjs";
 import { initPage as initProfilePage } from "../pages/profile/profile.mjs";
+import { singleListing } from "./handlers/SingleDisplay.mjs";
 
 import * as storage from "./storage/index.mjs";
+
 //import * as listings from "./listings/index.mjs"
 
 const path = location.pathname;
@@ -24,15 +26,17 @@ function checkRouting() {
         return;
     }
 
+    if (path === "/" || path.includes("single-listing")) {
+        singleListing("#singleListing");
+        return;
+    }
+
     if (path === "/" || path.includes("index")) {
         if (storage.isLoggedIn()) {
             getProfileListings("#myListingsOnly")
             listeners.listingFeed();
-            listeners.listingFeed("searchformMobile");
             listeners.setCreateListingFormListener("#createListing");
             listeners.setCreateListingFormListener("#createListingMobile");
-            //listeners.getProfileListings()
-            //searchformMobile
             listeners.allListings();
         } else {
             window.location.href = "/pages/public"
@@ -41,23 +45,3 @@ function checkRouting() {
 }
 
 window.onpaint = checkRouting();
-
-
-//listings.createListing({
-//    title: "testing to create listing & deleting",
-//    description: "test 2",
-//    endsAt: "2023-05-24",
-//    media: ["https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300"]
-//})
-
-
-//removeListing("f450b8c5-a1f4-49bf-86fa-2aa551b0b78e")
-
-//listings.updateAvatar({
-//    id: "04040e6a-9ca0-4fe1-8831-1bee8746aa0c",
-//    media: ["https://s3-us-west-2.amazonaws.com/assertible/integrations/swagger-logo-horizontal.jpeg"]
-//})
-
-//removeListing({
-//    id: "0f4162a3-75b9-47da-9246-d8277ed672bb"
-//})
