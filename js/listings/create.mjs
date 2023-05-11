27be9c5a2d2ee1576d3d251fdd74b7cde8633b3f
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-05-11 15:28:23 +0200

    adding try/catch to create listing

--- js/listings/create.mjs
+++ js/listings/create.mjs
@@ -7,31 +7,41 @@ const method = "post";
 
 
 export async function createListing(listingData) {
-    const createListingURL = API_AUCTION_URL + action;
-    // eslint-disable-next-line no-unused-vars
-    const token = load("token");
-    //const mediaArray = listingData.media.split(",");
-    // use split and/or index of to fix Date/time
+    try {
+        const createListingURL = API_AUCTION_URL + action;
+        // eslint-disable-next-line no-unused-vars
+        const token = load("token");
+        //const mediaArray = listingData.media.split(",");
+        // use split and/or index of to fix Date/time
 
-    const response = await authFetch(createListingURL, {
-        method,
-        body: JSON.stringify({
-            title: listingData.title,
-            description: listingData.description,
-            media: listingData.media,
-            endsAt: listingData.endsAt
-        }),
-    });
+        const response = await authFetch(createListingURL, {
+            method,
+            body: JSON.stringify({
+                title: listingData.title,
+                description: listingData.description,
+                media: listingData.media,
+                endsAt: listingData.endsAt
+            }),
+        });
+        const listing = await response.json();
+        console.log(listing);
+        if (!response.ok) {
+            alert("Something went wrong! please check that you filled out the required inputs, and check that your image/s have a valid URL")
+        } else {
+            alert("You just listed an Item!")
 
-    if (listingData.media) {
-        const img = document.createElement("img");
-        img.src = listingData.media;
-        img.alt = `Image from ${listingData.title}`;
+        }
+
+        return listing;
+    } catch (error) {
+        console.log(error);
     }
 
-    const listing = await response.json();
 
-    console.log(listing);
 
-    alert("You just listed an Item!")
+
+
+
+
 }
+
