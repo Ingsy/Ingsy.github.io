df5e62d893797289024268194039996b719acf82
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-05-15 08:34:39 +0200

    update single-listing

--- js/handlers/SingleDisplay.mjs
+++ js/handlers/SingleDisplay.mjs
@@ -1,49 +1,48 @@
 import { displayBids } from "./displayListings.mjs";
 import { userName } from "../api/constants.mjs";
+import { BidOnListing } from "../listings/bid.mjs";
 //import { makeBid } from "./displayListings.mjs";
-//import { getListing } from "../listings/read.mjs";
+import { getListing } from "../listings/read.mjs";
 
+
+export function displayMedia(media) {
+  console.log(media);
+  let mediaHtml = "";
+  for (let i = 0; i < media.length && i < 3; i++) {
+    const url = media[i];
+    mediaHtml += `<img src="${url}" class="single-listing-image" alt="..." />`;
+  }
+  return mediaHtml;
+}
 
 
 export async function displaySingleListing(listings, containerId) {
-  const listingContainer = document.querySelector(containerId);
-
-  listingContainer.innerHTML = "";
-  for (let i = 0; i < listings.lenght; i++) {
+  const listingContainerSingle = document.querySelector(containerId);
+  listingContainerSingle.innerHTML = ""; //cannot read property "lenght" of null!
+  for (let i = 0; i < listings.length; i++) {
     let listingHTML = "";
 
     listingHTML += `
-div class="col-sm-12 col-md-6 col-lg-6">
-            <h1>${listings[i].title}</h1>
+<div class="col-sm-12" id="listItem_${listings[i].id}">
             <div class="card card-single-listing mx-auto align-items-center">
               <h5 class="card-title mt-3 mb-0">${listings[i].title}</h5>
-
-             
               <div class="card-body">
-                <div class="card-div2 mb-3">
-                <div>
-                  <img src="${listings[i].media}" class="card-img-bid-small rounded float-left d-block" alt="...">
-                  <img src="${listings[i].media}" class="card-img-bid-small rounded float-left d-block" alt="...">
-                </div> 
-                <div>
-                  <img src="${listings[i].media}" class="rounded float-right card-img-bid-big d-block" alt="....">
+                <div class ="row image-container">`;
+    listingHTML += displayMedia(listings[i].media);
+    listingHTML += `
                 </div>
-              </div>
               <hr class="mt-1 mb-2" />
-                
-                
                 <div class="card-div m-3">
                   <h3>${listings[i].seller.name}</h3>
                   <h3>bidding ends:${listings[i].endsAt}</h3>
                 </div>`;
     if (listings[i].description) {
-      listingHTML += ` <hr class="mt-1 mb-2" />
-description: 
- <p class="text-start mt-2">${listings[i].description}</p> 
-<hr class="mt-1 mb-2" />`;
+      listingHTML += `<hr class="mt-1 mb-2" />
+                description: 
+                <p class="text-start mt-2">${listings[i].description}</p> 
+                <hr class="mt-1 mb-2" />`;
     }
     listingHTML += `          
-
                 <button
                 class="btn"
                 type="button"
@@ -98,15 +97,27 @@ description:
             </div>
           </div>
         </div> `;
-    listingContainer.innerHTML += listingHTML;
+    listingContainerSingle.innerHTML += listingHTML;
   }
 }
 
+export async function makeBid(event, id) {
+  event.preventDefault();
+  const amountInput = document.querySelector(`#bidAmount_${id}`);
+  await BidOnListing(id, parseInt(amountInput.value))
 
-export async function singleListing(listing) {
-  //const url = new URL(location.href);
-  //const id = url.searchParams.get("id");
-  //let listing = await getListing(id);
-  displaySingleListing([listing], "#singleListing");
+  const listingData = await getListing(id);
+  displaySingleListing([listingData], "#singleListing");
+  return false;
+}
+
+window.makeBid = makeBid;
+
+
+export async function singleListing() {
+  const url = new URL(location.href);
+  const id = url.searchParams.get("id");
+  let listingData = await getListing(id);
+  displaySingleListing([listingData], "#singleListing");
 
 }
