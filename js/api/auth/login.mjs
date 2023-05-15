c54658c7d35329121fae5851d98c29141c0e5989
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-05-15 13:39:56 +0200

    Update login.mjs

--- js/api/auth/login.mjs
+++ js/api/auth/login.mjs
@@ -6,29 +6,33 @@ const action = "/auth/login";
 const method = "post";
 
 export async function login(profile, userName) {
-    const loginURL = API_AUCTION_URL + action;
-    const body = JSON.stringify(profile, userName);
+    try {
+        const loginURL = API_AUCTION_URL + action;
+        const body = JSON.stringify(profile, userName);
 
-    const response = await fetch(loginURL, {
-        headers: {
-            "content-Type": "application/json",
-        },
-        method,
-        body,
-    });
+        const response = await fetch(loginURL, {
+            headers: {
+                "content-Type": "application/json",
+            },
+            method,
+            body,
+        });
 
-    const result = await response.json();
-    storage.saveString("token", result.accessToken);
-    storeProfile(result);
-    if (result.accessToken !== undefined && response.ok) {
-        alert("You are now logged in");
-        setTimeout(function () {
-            window.location.href = "/pages/profile";
-        }, 2500);
+        const result = await response.json();
+        storage.saveString("token", result.accessToken);
+        storeProfile(result);
+        if (result.accessToken !== undefined && response.ok) {
+            alert("You are now logged in");
+            setTimeout(function () {
+                window.location.href = "/pages/profile";
+            }, 2000);
 
-    } else if (result.accessToken === undefined) {
-        alert("Sign in unsuccessful. Please try again or register an account")
+        } else if (result.accessToken === undefined) {
+            alert("Sign in unsuccessful. Please try again or register an account")
+        }
+
+        return result;
+    } catch (error) {
+        console.log(error)
     }
-    // eslint-disable-next-line no-undef
-    return json;
 }
