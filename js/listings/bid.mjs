4dd17b27751ce2541a3fdef872e21dbdf6c99e2f
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-05-18 09:34:12 +0200

    updates

--- js/listings/bid.mjs
+++ js/listings/bid.mjs
@@ -9,21 +9,27 @@ const method = "post";
 
 
 export async function BidOnListing(listingId, amount) {
+    try {
+        const BidOnListingURL = `${API_AUCTION_URL}${action}/${listingId}/bids`;
 
+        const response = await authFetch(BidOnListingURL, {
+            method,
+            body: JSON.stringify({
+                amount: amount
+            }),
+        });
+        const bid = await response.json();
+        if (!response.ok) {
+            alert("something went wrong! please make sure You are logged in, the listing is active and that you have enough credit")
+        } else {
+            alert("you are currently the higest bidder. good luck")
+        }
 
-
-    if (!listingId) {
-        throw new Error("bidding requires ID");
+        return bid;
+    } catch (error) {
+        console.log(error)
     }
-    const BidOnListingURL = `${API_AUCTION_URL}${action}/${listingId}/bids`;
 
-    const response = await authFetch(BidOnListingURL, {
-        method,
-        body: JSON.stringify({
-            amount: amount
-        }),
-    });
-    return await response.json();
 }
 
 
