6aa398d2c337e517b5819c288585cf9dfeba6b4f
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-05-19 17:08:46 +0200

    fixing base url typo

--- js/api/constants.mjs
+++ js/api/constants.mjs
@@ -1,4 +1,4 @@
-export const API_HOST_URL = "https:/api.noroff.dev";
+export const API_HOST_URL = "https://api.noroff.dev";
 export const API_BASE_URL = "/api/v1";
 export const API_AUCTION_BASE = "/auction";
 export const API_AUCTION_URL = `${API_HOST_URL}${API_BASE_URL}${API_AUCTION_BASE}`;
